bcc470c0febde8ce3ed42627371944ad770a8586
chayrrou <chayrrou.mariem@gmail.com>
2022-11-19 00:22:35 -0400

    faire la routage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NotFound404Component } from './components/not-found404/not-found404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',title:'Home', component:HomeComponent},
+  {path:'menu',title:'Menu', component:MenuComponent},
+  {path:'aboutUs',title:'AboutUs', component:AboutUsComponent},
+  {path:'contact',title:'Contact', component:ContactComponent},
+  {path:'login', title:'Login',component:LoginComponent},
+  {path:'**', title:'NotFound',component:NotFound404Component}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component'; 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon'; 
+import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotFound404Component } from './components/not-found404/not-found404.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/login/login.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutUsComponent,
+    FooterComponent,
+    NotFound404Component,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
